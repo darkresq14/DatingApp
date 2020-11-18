3c946d500f54e638f3adaa913f2b583cd4fe254c
dArKrEsQ <razvan.bielz@gmail.com>
2020-11-18 18:43:38 +0100

    End of section 6 implemented routing

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,7 +21,10 @@ export class RegisterComponent implements OnInit {
   register() {
     this.accountService.register(this.model).subscribe(response => {
       this.cancel();
-    }, error => console.log(error));
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    });
   }
 
   cancel() {
