54f5f4b596bc81a5acd0075499bdfb51b38d0378
dArKrEsQ <razvan.bielz@gmail.com>
2020-11-19 20:05:24 +0100

    End of section 8 Extending the API

---
+++ API/Data/Migrations/20201119163808_Typo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class Typo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ismain",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "Ismain");
+        }
+    }
+}
